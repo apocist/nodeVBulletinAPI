7dadea7a7af09cf4b8e7df5979d284b6106cbc77
Apocist <apocist@yahoo.com>
2019-05-13 17:22:53 -0700

    Converted into modern class system

--- Thread.js
+++ Thread.js
@@ -1,61 +1,64 @@
 "use strict";
 const Post = require('./Post');
 
-/**
- *
- * @param {object} rawData
- * @constructor
- * @property {number} forumId
- * @property {string} forumTitle
- * @property {number} threadId
- * @property {string} title
- * @property {[Post]} posts
- */
-const Thread = function Thread(rawData) {
-	this.rawData = rawData;
-	this.parseData();
-	this.cleanup();
-};
+class Thread {
+	/**
+	 * @typedef RawThreadData
+	 * @property {number} forumId
+	 * @property {string} forumTitle
+	 * @property {number} threadId
+	 * @property {string} title
+	 * @property {Post[]} posts
+	 */
 
+	/**
+	 * @param {RawThreadData} rawData
+	 */
+	constructor (rawData){
+		this.rawData = rawData;
+		this.parseData();
+		this.cleanup();
+	}
 
-Thread.prototype.parseData = function() {
-	if(this.rawData) {
-		//TODO need to speficiy if its fully fetched
-		let rawData = this.rawData;
-		
-		if(rawData.hasOwnProperty('thread')) {
-			let threadData = rawData['thread'];
-			if (threadData.hasOwnProperty('forumid')) {
-				this.forumId = parseInt(threadData.forumid);
-			}
-			if (threadData.hasOwnProperty('forumtitle')) {
-				this.forumTitle = threadData.forumtitle;
-			}
-			if (threadData.hasOwnProperty('threadid')) {
-				this.threadId = parseInt(threadData.threadid);
-			}
-			if (threadData.hasOwnProperty('title')) {
-				this.title = threadData.title;
-			} else if (threadData.hasOwnProperty('threadtitle')) {
-				this.title = threadData.threadtitle;
-			}
-		}
-		
-		if (rawData.hasOwnProperty('postbits')) {
-			let postBits = rawData.postbits;
-			this.posts = [];
-			for (let post in postBits) {
-				if (postBits.hasOwnProperty(post)) {
-					this.posts.push(new Post(postBits[post]));
+	parseData() {
+		if(this.rawData) {
+			//TODO need to specify if its fully fetched
+			let rawData = this.rawData;
+
+			if(rawData.hasOwnProperty('thread')) {
+				let threadData = rawData['thread'];
+				if (threadData.hasOwnProperty('forumid')) {
+					this.forumId = parseInt(threadData.forumid);
+				}
+				if (threadData.hasOwnProperty('forumtitle')) {
+					this.forumTitle = threadData.forumtitle;
+				}
+				if (threadData.hasOwnProperty('threadid')) {
+					this.threadId = parseInt(threadData.threadid);
+				}
+				if (threadData.hasOwnProperty('title')) {
+					this.title = threadData.title;
+				} else if (threadData.hasOwnProperty('threadtitle')) {
+					this.title = threadData.threadtitle;
 				}
 			}
-		}
-		
-	}
-};
 
-Thread.prototype.cleanup = function() {
-	delete(this.rawData);
-};
+			if (rawData.hasOwnProperty('postbits')) {
+				let postBits = rawData.postbits;
+				this.posts = [];
+				for (let post in postBits) {
+					if (postBits.hasOwnProperty(post)) {
+						this.posts.push(new Post(postBits[post]));
+					}
+				}
+			}
+
+		}
+	}
+
+	cleanup() {
+		delete(this.rawData);
+	}
+}
 
 module.exports = Thread;
